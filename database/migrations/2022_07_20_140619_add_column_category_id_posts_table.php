c5bb2164f0b8b3236e3c5132169024f5014a6c12
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-07-20 16:21:28 +0200

    aggiunta colonna foreign key e collegato le due tabelle

--- database/migrations/2022_07_20_140619_add_column_category_id_posts_table.php
+++ database/migrations/2022_07_20_140619_add_column_category_id_posts_table.php
@@ -14,7 +14,7 @@ class AddColumnCategoryIdPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            
+            $table->foreignId('category_id')->nullable()->after('published')->constrained()->onDelete('set null');
         });
     }
 
@@ -26,7 +26,8 @@ class AddColumnCategoryIdPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
         });
     }
 }
